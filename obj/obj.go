cd4e56c08488d2bbd3aa05ecdd6e7995a628f6a1
Nicholas Blaskey <nicholas.blaskey1@marist.edu>
2022-01-17 13:02:13 -0500

    Added parsing support for vertices and faces

--- obj/obj.go
+++ obj/obj.go
@@ -2,15 +2,22 @@ package obj
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
+
+	"github.com/nicholasblaskey/raytracer/shape"
+	"github.com/nicholasblaskey/raytracer/tuple"
 )
 
 type Parser struct {
 	LinesIgnored int
+	Vertices     []tuple.Tuple
+	DefaultGroup *shape.Group
 }
 
-func Parse(s string) *Parser {
-	linesIgnored := 0
+func Parse(s string) (*Parser, error) {
+	p := &Parser{DefaultGroup: shape.NewGroup()}
+
 	for _, line := range strings.Split(s, "\n") {
 		fmt.Println(line)
 		line = strings.Trim(line, "\n\t\r ")
@@ -18,13 +25,42 @@ func Parse(s string) *Parser {
 			continue
 		}
 
-		switch {
+		switch line[0] {
+		case 'v':
+			var err error
+			verts := strings.Split(line[2:], " ")
+			vert := tuple.Point(0.0, 0.0, 0.0)
+
+			if vert[0], err = strconv.ParseFloat(verts[0], 64); err != nil {
+				return nil, err
+			}
+			if vert[1], err = strconv.ParseFloat(verts[1], 64); err != nil {
+				return nil, err
+			}
+			if vert[2], err = strconv.ParseFloat(verts[2], 64); err != nil {
+				return nil, err
+			}
+			p.Vertices = append(p.Vertices, vert)
+		case 'f':
+			vertStrings := strings.Split(line[2:], " ")
+			var verts []int
+			for _, vertString := range vertStrings {
+				v, err := strconv.Atoi(vertString)
+				if err != nil {
+					return nil, err
+				}
+				verts = append(verts, v-1)
+			}
+
+			if len(verts) == 3 {
+				p.DefaultGroup.AddChild(shape.NewTriangle(
+					p.Vertices[verts[0]], p.Vertices[verts[1]], p.Vertices[verts[2]]),
+				)
+			}
 		default:
-			linesIgnored++
+			p.LinesIgnored++
 		}
 	}
 
-	return &Parser{
-		LinesIgnored: linesIgnored,
-	}
+	return p, nil
 }
--- test/obj_test.go
+++ test/obj_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 
 	"github.com/nicholasblaskey/raytracer/obj"
-	//"github.com/nicholasblaskey/raytracer/matrix"
+	"github.com/nicholasblaskey/raytracer/shape"
 
 	"github.com/cucumber/godog"
 )
@@ -26,14 +26,33 @@ func objSteps(ctx *godog.ScenarioContext) {
 
 	ctx.Step(fmt.Sprintf(`^%s should have ignored %s lines$`, wordRegex, intRegex),
 		parserShouldIgnoreLines)
+
+	ctx.Step(fmt.Sprintf(`^%s.vertices\[%s\] = point\(%s, %s, %s\)$`,
+		wordRegex, intRegex, floatRegex, floatRegex, floatRegex),
+		parserVertexAtEqualTo)
+
+	ctx.Step(fmt.Sprintf(`^%s ← %s.default_group$`, wordRegex, wordRegex),
+		assignParserDefaultGroup)
+	ctx.Step(fmt.Sprintf(`^%s ← (first|second) child of %s$`,
+		wordRegex, wordRegex), assignChildOfGroup)
+
+	ctx.Step(fmt.Sprintf(`^%s.%s = %s.vertices\[%s\]$`,
+		wordRegex, wordRegex, wordRegex, intRegex),
+		trianglePointEqualToParserVert)
 }
 
 func createFileString(f string, contents *godog.DocString) {
 	fileStrings[f] = string(contents.Content)
 }
 
-func parseObjectFile(p, s string) {
-	objParsers[p] = obj.Parse(fileStrings[s])
+func parseObjectFile(p, s string) error {
+	parser, err := obj.Parse(fileStrings[s])
+	if err != nil {
+		return err
+	}
+
+	objParsers[p] = parser
+	return nil
 }
 
 func parserShouldIgnoreLines(p string, expected int) error {
@@ -42,4 +61,54 @@ func parserShouldIgnoreLines(p string, expected int) error {
 		return fmt.Errorf("%s.LinesIgnored expected %d got %d", p, expected, actual)
 	}
 	return nil
+}
+
+func parserVertexAtEqualTo(p string, i int, x, y, z float64) error {
+	i-- // Tests assume index 1 based scheme
+	if i >= len(objParsers[p].Vertices) {
+		return fmt.Errorf("%s.vertices[%d] out of bounds (len(%s.vertexes) = %d)",
+			p, i, p, len(objParsers[p].Vertices))
+	}
+
+	actual := fmt.Sprintf("%s.vertices[%d]", p, i)
+	tuples[actual] = objParsers[p].Vertices[i]
+
+	return isEqualTuple(actual, x, y, z, tuples[actual][3])
+}
+
+func assignParserDefaultGroup(g, p string) {
+	shapes[g] = objParsers[p].DefaultGroup
+}
+
+func assignChildOfGroup(res, nth, g string) error {
+	i := 0
+	switch nth {
+	case "first":
+		i = 0
+	case "second":
+		i = 1
+	default:
+		return fmt.Errorf("Should not happen")
+	}
+
+	children := shapes[g].(*shape.Group).Children
+	if i >= len(children) {
+		return fmt.Errorf("out of bounds wanted %d element of %s.children (len = %d)",
+			i, g, len(children))
+	}
+
+	shapes[res] = children[i]
+	return nil
+}
+
+func trianglePointEqualToParserVert(tri, point, parser string, i int) error {
+	i--
+	if i >= len(objParsers[parser].Vertices) {
+		return fmt.Errorf("%s.vertices[%d] out of bounds (len(%s.vertexes) = %d)",
+			parser, i, parser, len(objParsers[parser].Vertices))
+	}
+
+	expected := fmt.Sprintf("%s.vertices[%d]", parser, i)
+	tuples[expected] = objParsers[parser].Vertices[i]
+	return trianglePointEqualTo(tri, point, expected)
 }
